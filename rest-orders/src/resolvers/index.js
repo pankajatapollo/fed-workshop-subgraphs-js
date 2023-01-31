17878bad37142aa1e805ccbb898eb0deadf6c10c
Pankaj Ahire SE <pankaj@apollographql.com>
2023-01-31 19:08:08 +0000

    Made changes per SE Workshop

--- rest-orders/src/resolvers/index.js
+++ rest-orders/src/resolvers/index.js
@@ -1,13 +1,35 @@
 const resolvers = {
-  Query: {
-    // @WORKSHOP 2.3.1: Create a Query resolver for order operation and return an order
-    order() {
-      return 
+Query: {
+    // Returns the order
+    order: (_, { id }, { dataSources }) => {
+      return dataSources.ordersAPI.getOrder(id);
     }
   },
-  // @WORKSHOP 2.3.2: Add a reference resolver for Order Entity
   Order: {
+    __resolveReference: (order, { dataSources }) => {
+      return dataSources.ordersAPI.getOrder(order.id);
+    },
+    buyer: (root) => {
+      const customerId = root.customerId;
+      return {
+        id: customerId
+      };
+    },
+    items: (root) => {
+      const variantIds = root.variantIds;
+      const items = [];
+
+      for (let index = 0; index < variantIds.length; index++) {
+        const item = {
+          id: variantIds[index]
+        };
+        items.push(item);
+      }
+
+      return items;
+    }
   }
+
 };
 
 module.exports = resolvers;
